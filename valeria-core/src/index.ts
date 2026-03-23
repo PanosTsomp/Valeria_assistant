a3e2accf373683eee8cf93b5aae8e0e07580797f
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2026-03-23 15:57:49 +0200

    Added SentenceBuffer to the index.ts "API"

--- valeria-core/src/index.ts
+++ valeria-core/src/index.ts
@@ -1,3 +1,3 @@
 export { Message, LLMConfig, STTEngine, LLMEngine, TTSEngine } from './types.js';
 export { Conversation, VALERIA_SYSTEM_PROMPT } from './conversation.js';
-
+export { SentenceBuffer } from './sentence-buffer.js';
