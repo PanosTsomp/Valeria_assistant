0f0a15427fcdec1f65d301c3538e26fddd070f7e
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2026-03-26 09:30:46 +0200

    Updated Index

--- valeria-core/src/index.ts
+++ valeria-core/src/index.ts
@@ -1,3 +1,5 @@
 export { Message, LLMConfig, STTEngine, LLMEngine, TTSEngine } from './types.js';
 export { Conversation, VALERIA_SYSTEM_PROMPT } from './conversation.js';
 export { SentenceBuffer } from './sentence-buffer.js';
+export { Pipeline, PipelineState, PipelineCallbacks } from './pipeline.js';
+
